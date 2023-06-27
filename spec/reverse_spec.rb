bed736f92fc1b2725657573f2348f4d32833f722
abeltsew <35835135+abeltsew@users.noreply.github.com>
2023-06-27 10:59:37 +0300

    Create spec for reversing a string

---
+++ spec/reverse_spec.rb
@@ -0,0 +1,9 @@
+require_relative '../reverse'
+
+
+describe ('#reverse') do
+    it('should reverse a word properly') do
+        reversed = reverse('hello')
+        expect(reversed).to eq('olleh')
+    end
+end
